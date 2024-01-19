1117d87262c0f252006d7811426c06a10eca8fa1
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-01-19 13:03:35 +0700

    Update cors

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 server.use(middlewares);
 server.use(router);
 server.listen(5000, () => {
